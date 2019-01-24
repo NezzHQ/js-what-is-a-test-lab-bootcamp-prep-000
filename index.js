b2fe65a1e3f5e3a9856d14acd5ebdde51ee11413
Bens Hilaire <benshilaire@gmail.com>
2019-01-24 07:29:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // write your code below!
 var name = "Susan"
-var height = 74 
+var height = 74 > 40;
 var message = ` ${name} is ${height} inches tall`
-if (height >40 )
+
 module.exports = {name, height, message}
 
 
